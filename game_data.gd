19952da3c9e2ec9835f6ffe42edc3bdfd1f8044b
Rachel Rosalia <rachel@rosalia.me>
2024-01-20 18:48:00 -0800

    Move artificial loading delay before status loop

--- game_data.gd
+++ game_data.gd
@@ -7,11 +7,11 @@ var arcade_position
 func start_game(game_path):
   get_tree().change_scene_to_packed(loading_scene)
   ResourceLoader.load_threaded_request(game_path)
+  await get_tree().create_timer(0.5).timeout
+  print_debug("artificial delay done")
   while ResourceLoader.load_threaded_get_status(game_path) == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
     await get_tree().create_timer(0.1).timeout
   print_debug("loaded!")
-  await get_tree().create_timer(0.5).timeout
-  print_debug("artificial delay done")
   var game_scene = ResourceLoader.load_threaded_get(game_path)
   get_tree().change_scene_to_packed(game_scene)
 
