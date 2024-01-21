e6ac7075f985810e98b94339fe4ebe2d570f1b0d
Rachel Rosalia <rachel@rosalia.me>
2024-01-20 16:06:10 -0800

    Add arcade cabinet selection

--- arcade_cabinet.gd
+++ arcade_cabinet.gd
@@ -4,7 +4,12 @@ extends Node2D
 @export var color : Color = Color(0,0,0):
   set(new_color):
     color = new_color
-    modulate = color
+    $Sprite.modulate = color
+    
+var selected = false:
+  set(new_selected):
+    selected = new_selected
+    $Selection.visible = selected
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- character.gd
+++ character.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 48.0
 
+var arcade_cabinets = []
+
 func _process(_delta):
   if Input.is_action_pressed("ui_right"):
     $sprite.play("walk_right")
@@ -14,7 +16,7 @@ func _process(_delta):
   else:
     $sprite.play("default")
 
-func _physics_process(delta):
+func _physics_process(_delta):
   if Input.is_action_pressed("ui_right"):
     velocity.x = SPEED
   elif Input.is_action_pressed("ui_left"):
@@ -30,3 +32,26 @@ func _physics_process(delta):
     velocity.y = 0
 
   move_and_slide()
+
+func _update_arcade_highlights():
+  for cabinet in arcade_cabinets:
+    cabinet.selected = false
+  if arcade_cabinets.size() > 0:
+    arcade_cabinets[-1].selected = true
+
+func _on_arcade_cabinet_entered(body: Node2D):
+  arcade_cabinets.append(body)
+  _update_arcade_highlights()
+
+
+func _on_arcade_cabinet_exited(body: Node2D):
+  body.selected = false
+  var index = arcade_cabinets.find(body)
+  if index != -1:
+    arcade_cabinets.remove_at(index)
+    _update_arcade_highlights()
+
+func _input(event):
+  if event.is_action_pressed("ui_accept"):
+    if arcade_cabinets.size() > 0:
+      arcade_cabinets[-1].selected = false
